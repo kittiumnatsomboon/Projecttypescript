a2f5ed50cb329672603762d2c9403a3b2c798101
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2026-02-13 11:56:33 +0700

    ลบเทม

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
-import { ThemeProvider } from "next-themes";
+import Navbar from './component/navbar';
 
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
-import { PopupWidget }  from "./components/PopupWidget";
-function Layout() {
-  return (
-    <ThemeProvider attribute="class">
-      <Navbar />
-      <div>
-        <Outlet />
-      </div>
-      <PopupWidget />
-    </ThemeProvider>
-  );
-}
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        <Route element={<Navbar />}>
           <Route path="/" element={<Home />} />
         </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
