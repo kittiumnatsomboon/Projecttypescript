0da81876d02e1b9c8c0d30a5209d64a808787b7c
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2026-02-13 13:39:14 +0700

    Templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+import { ThemeProvider } from "next-themes";
+
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { PopupWidget } from "./components/PopupWidget";
 import Home from './pages/Home';
-import Navbar from './component/navbar';
+
+
+function Layout() {
+  return (
+    <ThemeProvider attribute="class">
+      <Navbar />
+      <Footer />
+      <PopupWidget />
+    </ThemeProvider>
+  )
+}
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Navbar />}>
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
         </Route>
       </Routes>
