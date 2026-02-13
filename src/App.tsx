fd22f305e1a129d4d87201c1a847a124f916b3b0
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2026-02-13 10:37:14 +0700

    Revert "ลบปุ่ม Them สีขาวออก"
    This reverts commit 05e5679a6d616b6c530b7b61174bbbae1164d2f0, reversing
    changes made to 4cec71a81453f1b424c914b1aed1204012e13ac4.

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
 import React, { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
-import { ThemeProvider } from "next-themes";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
-import { PopupWidget } from "./components/PopupWidget";
 import Home from './pages/Home';
+import Navbar from './component/navbar';
 
-// layout
-function Layout() {
-  return (
-    <ThemeProvider attribute="class">
-      <Navbar />
-      <div>
-        <Outlet />
-      </div>
-      <PopupWidget />
-    </ThemeProvider>
-  );
-}
-
-function App({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
+          <Route path='/' element={<Navbar/>}>
+              <Route index element={<Home/>} />
+          </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   )
 }
