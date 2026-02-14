ff3dc4e9a3f0e8a1a14b8fb83d3d5aedd7583f21
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2026-02-14 20:39:43 +0700

    React Register + Formvalidate
    สร้างระบบสมัครสมาชิก + validate formik

--- src/authforms/AuthRegister.tsx
+++ src/authforms/AuthRegister.tsx
@@ -1,109 +1,154 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router";
 import * as Yup from 'yup';
+import { Field, Form, Formik, ErrorMessage } from "formik"
 import Modal from "react-modal";
-import Modalterm, { CustomStyles  } from "../components/Modal";
+import Modalterm, { CustomStyles } from "../components/Modal";
 // ยังไม่ได้แก้
 const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
+  fullname: Yup.string()
+    .min(2, 'อักษรขั้นต่ำ 2 ตัวอักษร')
+    .max(50, 'อักษรสูงสุด 50 ตัวอักษร')
+    .required('กรุณาระบุข้อมูล'),
   email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
+    .email('รูปแบบอีเมลล์ไม่ถูกต้อง')
+    .required('กรุณาระบุข้อมูล'),
+  password: Yup.string()
+    .min(2, 'อักษรขั้นต่ำ 2 ตัวอักษร')
+    .max(50, 'อักษรสูงสุด 50 ตัวอักษร')
+    .matches(/[a-z]/, 'รหัสผ่านต้องมีตัวอักษรตัวเล็กอย่างน้อย 1 ตัวอักษร')
+    .matches(/[A-Z]/, 'รหัสผ่านต้องมีตัวอักษรตัวใหญ่อย่างน้อย 1 ตัวอักษร')
+    .matches(/[0-9]/, 'รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัวอักษร')
+    .matches(/[^a-zA-Z0-9]/, 'ตัวอักษรต้องมีสัญลักษณ์พิเศษอย่างน้อย 1 ตัวอักษร')
+    .required('กรุณาระบุข้อมูล'),
+  confirmpassword: Yup.string()
+    .min(2, 'อักษรขั้นต่ำ 2 ตัวอักษร')
+    .max(50, 'อักษรสูงสุด 50 ตัวอักษร')
+    .matches(/[a-z]/, 'รหัสผ่านต้องมีตัวอักษรตัวเล็กอย่างน้อย 1 ตัวอักษร')
+    .matches(/[A-Z]/, 'รหัสผ่านต้องมีตัวอักษรตัวใหญ่อย่างน้อย 1 ตัวอักษร')
+    .matches(/[0-9]/, 'รหัสผ่านต้องมีตัวเลขอย่างน้อย 1 ตัวอักษร')
+    .matches(/[^a-zA-Z0-9]/, 'ตัวอักษรต้องมีสัญลักษณ์พิเศษอย่างน้อย 1 ตัวอักษร')
+    .oneOf([Yup.ref('password')], 'รหัสผ่านต้องตรงกันกับข้างบน')
+    .required('กรุณาระบุข้อมูล'),
+  agree: Yup.bool()
+    .oneOf([true], 'กรุณากดปุ่มยิมยอม')
+    .required('กรุณากดยิมยอม'),
 });
-
 
 
 
 const AuthRegister = () => {
   const [openmodalterm, setopenmodal] = useState(false);
   function openModal() {
-        setopenmodal(true);
-    }
-    function closeModal() {
-        setopenmodal(false);
-    }
-
-  const navigate = useNavigate();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(event);
-    navigate("/");
+    setopenmodal(true);
   }
+  function closeModal() {
+    setopenmodal(false);
+  }
+
+
 
   return (
     <>
-      <form onSubmit={handleSubmit} >
-        <div className="mb-4">
-          <div className="mb-2 block text-black">
-            <label htmlFor="fullname">ชื่อ-นามสกุล</label>
-          </div>
-          <input
-            id="fullname"
-            type="text"
-            placeholder="กรุณาระบุชื่อ-นามสกุล"
-            className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-4">
-          <div className="mb-2 block text-black">
-            <label htmlFor="email">ที่อยู่อีเมลล์</label>
-          </div>
-          <input
-            id="email"
-            type="email"
-            placeholder="กรุณาระบุที่อยู่อีเมล์"
-            className="px-3 py-2 border border-gray-300 text-black w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-
-        <div className="mb-4">
-          <div className="mb-2 block text-black">
-            <label htmlFor="password">รหัสผ่าน</label>
-          </div>
-          <input
-            id="password"
-            type="password"
-            placeholder="กรุณาระบุรหัสผ่าน"
-            className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-
-        <div className="mb-4">
-          <div className="mb-2 block text-black">
-            <label htmlFor="confirmpassword"></label>ยืนยันรหัสผ่าน
-          </div>
-          <input
-            id="confirmpassword"
-            type="password"
-            placeholder="กรุณาระบุรหัสผ่านยืนยันอีกครั้ง"
-            className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="inline-flex items-center">
-            <input type="checkbox" className="form-checkbox h-5 w-5 rounded focus:ring-blue-500" />
-            <span className="ml-2 text-black">
-              ข้าพเจ้าได้อ่านและยอมรับ <Link to="#" onClick={openModal} className="text-blue-500">[ข้อกำหนดและเงื่อนไขการใช้บริการ]</Link> 
-            </span>
-          </label>
-          {openmodalterm &&
-            <div>
-              <Modal 
-              isOpen={openmodalterm}
-              style={CustomStyles}
-              onRequestClose={closeModal}
-              >
-                <Modalterm/>
-              </Modal>
+      <Formik
+        initialValues={{
+          fullname: "",
+          email: "",
+          password: "",
+          confirmpassword: "",
+          agree:"",
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log('Submitting', values);
+          setSubmitting(false);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form >
+            <div className="mb-4">
+              <div className="mb-2 block text-black">
+                <label htmlFor="fullname">ชื่อ-นามสกุล</label>
+              </div>
+              <Field
+                id="fullname"
+                name="fullname"
+                type="text"
+                placeholder="กรุณาระบุชื่อ-นามสกุล"
+                className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              <ErrorMessage name="fullname" component="div" className="text-red-500" />
             </div>
-          }
-        </div>
-        <button type="submit" className="w-full bg-sky-500 rounded-sm pt-2 pb-2 hover:bg-sky-700">สมัครสมาชิก</button>
+            <div className="mb-4">
+              <div className="mb-2 block text-black">
+                <label htmlFor="email">ที่อยู่อีเมลล์</label>
+              </div>
+              <Field
+                id="email"
+                type="email"
+                name="email"
+                placeholder="กรุณาระบุที่อยู่อีเมล์"
+                className="px-3 py-2 border border-gray-300 text-black w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              <ErrorMessage name="email" component="div" className="text-red-500" />
 
-      </form>
+            </div>
+
+            <div className="mb-4">
+              <div className="mb-2 block text-black">
+                <label htmlFor="password">รหัสผ่าน</label>
+              </div>
+              <Field
+                id="password"
+                type="password"
+                name="password"
+                placeholder="กรุณาระบุรหัสผ่าน"
+                className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              <ErrorMessage name="password" component="div" className="text-red-500" />
+
+            </div>
+
+            <div className="mb-4">
+              <div className="mb-2 block text-black">
+                <label htmlFor="confirmpassword"></label>ยืนยันรหัสผ่าน
+              </div>
+              <Field
+                id="confirmpassword"
+                name="confirmpassword"
+                type="password"
+                placeholder="กรุณาระบุรหัสผ่านยืนยันอีกครั้ง"
+                className="px-3 py-2 border text-black border-gray-300 w-full rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+              <ErrorMessage name="confirmpassword" component="div" className="text-red-500" />
+
+            </div>
+            <div className="mb-4">
+              <label className="inline-flex items-center">
+                <Field type="checkbox" name="agree" className="form-checkbox h-5 w-5 rounded focus:ring-blue-500" />
+                <span className="ml-2 text-black">
+                  ข้าพเจ้าได้อ่านและยอมรับ <Link to="#" onClick={openModal} className="text-blue-500">[ข้อกำหนดและเงื่อนไขการใช้บริการ]</Link>
+                </span>
+              </label>
+              <ErrorMessage name="agree" component="div" className="text-red-500" />
+              
+
+              {openmodalterm &&
+                <div>
+                  <Modal
+                    isOpen={openmodalterm}
+                    style={CustomStyles}
+                    onRequestClose={closeModal}
+                  >
+                    <Modalterm />
+                  </Modal>
+                </div>
+              }
+            </div>
+            <button type="submit" className="w-full bg-sky-500 rounded-sm pt-2 pb-2 hover:bg-sky-700">สมัครสมาชิก</button>
+          </Form>
+        )}
+      </Formik>
     </>
   )
 }
